b1eec204711e992f68172d5a6b8c067c58f3d370
Anastasiya <anastasiya.bukhala@bouvet.no>
2020-04-23 15:28:09 +0200

    Partials added + project reorganization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Footer from 'components/partials/Footer'
+
+class App extends Component {
+  render() {
+    return (
+      <div id="colorlib-page">
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/partials/Footer.js
@@ -0,0 +1,43 @@
+// Dependencies
+import React, { Component } from 'react';
+
+
+export default class Footer extends Component {
+
+    render() {
+        return (
+            <div class="container-fluid text-center text-md-left">
+                <div class="row">
+                    <div class="col-md-6 my-3 contact-info">
+                        <h5>Kontakt</h5>
+                        <div>Villa Norangdal</div>
+                        <div>6218 Hellesylt</div>
+                        <div>70 26 10 84</div>
+                        <div>mail@norangdal.com</div>
+                    </div>
+                    <div class="col-md-3 my-3 follow-footer">
+                        <h5>Følg oss</h5>
+                        <a href="https://instagram.com/villanorangdal" target="_blank">
+                            <instagram></instagram>
+                        </a>
+                        <a href="https://www.facebook.com/norangdal/" target="_blank">
+                            <facebook></facebook>
+                        </a>
+                        <a href="https://www.tripadvisor.com/Hotel_Review-g1509004-d1501470-Reviews-Villa_Norangdal-Hellesylt_Stranda_More_og_Romsdal_Western_Norway.html" target="_blank">
+                            <tripadvisor></tripadvisor>
+                        </a>
+                    </div>
+                    <div class="col-md-3 pt-3">
+                        <a class="float-right" href="http://www.olavsrosa.no" target="_blank">
+                            <img src="/images/logo_olavs_rose.png" height="30px;" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+
