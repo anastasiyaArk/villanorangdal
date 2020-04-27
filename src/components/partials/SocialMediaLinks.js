c3908e4891db955134fb89f0c1c905c30ea4dee4
Anastasiya <anastasiya.bukhala@bouvet.no>
2020-04-27 15:07:45 +0200

    Component stylesheet added

--- src/components/partials/SocialMediaLinks.js
+++ src/components/partials/SocialMediaLinks.js
@@ -1,32 +1,26 @@
 // Dependencies
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// Stylesheets
+import style from 'components/partials/SocialMediaLinks.module.scss';
 
 class SocialMediaLinks extends Component {
 
   render() {
     return (
-    <div>
-      <a href='https://www.facebook.com/DehliMusikk/' title='Link to Facebook page' aria-label='Link to Facebook page' target='_blank' rel="noopener noreferrer" >
-        <FontAwesomeIcon icon={['fab', 'facebook-f']} alt='Facebook logo'/>
-      </a>
-      <a href='https://www.instagram.com/benjamindehli/' title='Link to Instagram page' aria-label='Link to Instagram page' target='_blank' rel="noopener noreferrer">
-        <FontAwesomeIcon icon={['fab', 'instagram']} alt='Instagram logo'/>
-      </a>
-      <a href='https://www.youtube.com/c/BenjaminDehli' title='Link to Youtube channel' aria-label='Link to Youtube channel' target='_blank' rel="noopener noreferrer">
-        <FontAwesomeIcon icon={['fab', 'youtube']} alt='Youtube logo'/>
-      </a>
-      <a href='https://vimeo.com/benjamindehli' title='Link to Vimeo page' aria-label='Link to Vimeo page' target='_blank' rel="noopener noreferrer">
-        <FontAwesomeIcon icon={['fab', 'vimeo-v']} alt='Vimeo logo'/>
-      </a>
-      <a href='https://benjamindehli.tumblr.com/' title='Link to Tumblr page' aria-label='Link to Tumblr page' target='_blank' rel="noopener noreferrer">
-        <FontAwesomeIcon icon={['fab', 'tumblr']} alt='Tumblr logo'/>
-      </a>
-      <a href='https://twitter.com/BenjaminDehli' title='Link to Twitter page' aria-label='Link to Twitter page' target='_blank' rel="noopener noreferrer">
-        <FontAwesomeIcon icon={['fab', 'twitter']} alt='Twitter logo'/>
-      </a>
-    </div>)
+      <div className={style.socialMediaLinks}>
+        <a href='https://www.facebook.com/norangdal/' title='Link to Facebook page' aria-label='Link to Facebook page' target='_blank' rel="noopener noreferrer" >
+          <FontAwesomeIcon icon={['fab', 'facebook-f']} alt='Facebook logo' />
+        </a>
+        <a href='href="https://instagram.com/villanorangdal' title='Link to Instagram page' aria-label='Link to Instagram page' target='_blank' rel="noopener noreferrer">
+          <FontAwesomeIcon icon={['fab', 'instagram']} alt='Instagram logo' />
+        </a>
+        <a href='https://www.tripadvisor.com/Hotel_Review-g1509004-d1501470-Reviews-Villa_Norangdal-Hellesylt_Stranda_More_og_Romsdal_Western_Norway.html' title='Link to Tripadvisor' aria-label='Link to Youtube channel' target='_blank' rel="noopener noreferrer">
+          <FontAwesomeIcon icon={['fab', 'tripadvisor']} alt='Tripadvisor logo' />
+        </a>
+      </div>)
   }
 }
 
