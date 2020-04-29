0319d37ffe78d4e83ac2b5852b928b58a3536b3c
Anastasiya <anastasiya.bukhala@bouvet.no>
2020-04-29 14:49:45 +0200

    Footer styling

--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -1,39 +1,51 @@
 // Dependencies
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 //Assests
 import LogoOlavsRose from 'assets/images/logo_olavs_rose.png'
 
-export default class Footer extends Component {
-    constructor(props){
+//Styles
+import style from 'components/partials/Footer.module.scss';
+
+class Footer extends Component {
+    constructor(props) {
         super(props);
         console.log(this.props); // prints out whatever is inside props
 
     }
     render() {
         return (
-            <div class="container-fluid text-center text-md-left">
-                <div class="row">
-                    <div class="col-md-6 my-3 contact-info">
-                        <h5>Kontakt</h5>
-                        <div>Villa Norangdal</div>
-                        <div>6218 Hellesylt</div>
-                        <div>70 26 10 84</div>
-                        <div>mail@norangdal.com</div>
-                    </div>
-                    <div class="col-md-3 my-3 follow-footer">
-                      
-                    </div>
-                    <div class="col-md-3 pt-3">
-                        <a class="float-right" href="http://www.olavsrosa.no" target="_blank">
-                            <img src={LogoOlavsRose} height="30px;" />
-                        </a>
+            <div className={style.footer}>
+                <div className={style.contentSection}>
+                    <div className={style.grid}>
+                        <div>
+                            <h5>Kontakt</h5>
+                            <div>Villa Norangdal</div>
+                            <div>6218 Hellesylt</div>
+                            <div>70 26 10 84</div>
+                            <div>mail@norangdal.com</div>
+                        </div>
+                        <div>
+
+                        </div>
+                        <div>
+                            <a href="http://www.olavsrosa.no" target="_blank">
+                                <img src={LogoOlavsRose} height="30px;" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = null;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
 
 
 
