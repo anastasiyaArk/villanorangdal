36321a3796776d6aba1f9594b20fde1f97266658
Anastasiya <anastasiya.bukhala@bouvet.no>
2020-05-04 10:42:55 +0200

    Carousel component was edited

--- src/components/partials/Carousel.js
+++ src/components/partials/Carousel.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import { MDBCarousel, MDBCarouselInner, MDBCarouselCaption, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
     "mdbreact";
+import { connect } from 'react-redux';
 
 // Actions
 import { getLanguageSlug, updateMultilingualRoutes, updateSelectedLanguageKey } from 'actions/LanguageActions';
@@ -14,63 +15,71 @@ import boutikBar from '../../assets/images/boutikBar.jpg';
 //Styles
 import style from 'components/partials/Carousel.module.scss';
 
-class Carousel extends Component{
-    render(){
-    return (
-        <MDBContainer>
-            <MDBCarousel
-                activeItem={1}
-                length={3}
-                showControls={true}
-                showIndicators={true}
-                className="z-depth-1"
-                slide
-            >
-                <MDBCarouselInner>
-                    <MDBCarouselItem itemId="1">
-                        <MDBView>
-                            <img
-                                className="d-block w-100"
-                                src={villaNorangdal}
-                                alt="Villa Norangdal"
-                            />
-                        </MDBView>
-                        <MDBCarouselCaption className={style.captionArea}>
-                            <h3 className={style.textContainer}>Velkommen til Villa Norangdal</h3>
-                            <p>_________</p>
-                        </MDBCarouselCaption>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId="2">
-                        <MDBView>
-                            <img
-                                className="d-block w-100"
-                                src={activity}
-                                alt="Activity"
-                            />
-                        </MDBView>
-                        <MDBCarouselCaption>
-                            <h3 className="h3-responsive">Rough Luxe - Litt røft og litt liksus</h3>
-                            <p>__________</p>
-                        </MDBCarouselCaption>
-                    </MDBCarouselItem>
-                    <MDBCarouselItem itemId="3">
-                        <MDBView>
-                            <img
-                                className="d-block w-100"
-                                src={boutikBar}
-                                alt="Hellesylt Boutik and Bar"
-                            />
-                        </MDBView>
-                        <MDBCarouselCaption>
-                            <h3 className="h3-responsive">Besøk Hellesylt Boutique & Bar</h3>
-                            <p>___________</p>
-                        </MDBCarouselCaption>
-                    </MDBCarouselItem>
-                </MDBCarouselInner>
-            </MDBCarousel>
-        </MDBContainer>
-    );
-}
+class Carousel extends Component {
+    render() {
+        return (
+            <MDBContainer>
+                <MDBCarousel
+                    activeItem={1}
+                    length={3}
+                    showControls={true}
+                    showIndicators={true}
+                    className="z-depth-1"
+                    slide
+                >
+                    <MDBCarouselInner>
+                        <MDBCarouselItem itemId="1">
+                            <MDBView>
+                                <img
+                                    className="d-block w-100"
+                                    src={villaNorangdal}
+                                    alt="Villa Norangdal"
+                                />
+                            </MDBView>
+                            <MDBCarouselCaption className={style.captionArea}>
+                                <h3 className={style.textContainer}>{this.props.selectedLanguageKey === 'en' ? 'Welcome to Villa Norangdal' : 'Velkommen til Villa Norangdal'}</h3>
+                                <p className={style.textContainer}>_________</p>
+                            </MDBCarouselCaption>
+                        </MDBCarouselItem>
+                        <MDBCarouselItem itemId="2">
+                            <MDBView>
+                                <img
+                                    className="d-block w-100"
+                                    src={activity}
+                                    alt={this.props.selectedLanguageKey === 'en' ? 'Activity' : 'Aktivitet'}
+                                />
+                            </MDBView>
+                            <MDBCarouselCaption className={style.captionArea}>
+                                <h3 className={style.textContainer}>{this.props.selectedLanguageKey === 'en' ? 'Rough Luxe: Some roughness and some luxury' : 'Rough Luxe - Litt røft og litt liksus'}</h3>
+                                <p className={style.textContainer}>__________</p>
+                            </MDBCarouselCaption>
+                        </MDBCarouselItem>
+                        <MDBCarouselItem itemId="3">
+                            <MDBView>
+                                <img
+                                    className="d-block w-100"
+                                    src={boutikBar}
+                                    alt="Hellesylt Boutik and Bar"
+                                />
+                            </MDBView>
+                            <MDBCarouselCaption className={style.captionArea}>
+                                <h3 className={style.textContainer}>{this.props.selectedLanguageKey === 'en' ? 'Visit Hellesylt Boutique & Bar' : 'Besøk Hellesylt Boutique & Bar'}</h3>
+                                <p className={style.textContainer}>__________</p>
+                            </MDBCarouselCaption>
+                        </MDBCarouselItem>
+                    </MDBCarouselInner>
+                </MDBCarousel>
+            </MDBContainer>
+        );
+    }
 }
 
-export default Carousel;
+const mapStateToProps = state => ({ selectedLanguageKey: state.selectedLanguageKey });
+
+const mapDispatchToProps = {
+  getLanguageSlug,
+  updateMultilingualRoutes,
+  updateSelectedLanguageKey
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Carousel);
