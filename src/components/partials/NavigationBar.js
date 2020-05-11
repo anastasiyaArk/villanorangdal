c3295ef590e021f2501f7f7b0121c11fb23c5458
Anastasiya <anastasiya.bukhala@bouvet.no>
2020-05-11 10:16:54 +0200

    Navigation menu for mobile devices added

--- src/components/partials/NavigationBar.js
+++ src/components/partials/NavigationBar.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 
 // Assets
 import { ReactComponent as NorangdalVillaLogo } from 'assets/images/logo.svg'
+import { ReactComponent as MenuIcon } from 'assets/images/menuIcon.svg'
+
 
 // Styles
 import style from 'components/partials/NavigationBar.module.scss'
@@ -13,11 +15,69 @@ import style from 'components/partials/NavigationBar.module.scss'
 import { getLanguageSlug } from '../../actions/LanguageActions';
 
 
+
+
 class NavigationBar extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showSidebar: false,
+            hidingSidebar: false,
+            showLanguageSelectorList: false
+        };
         this.setLanguageSelectorListWrapperRef = this.setLanguageSelectorListWrapperRef.bind(this);
+        this.setSidebarWrapperRef = this.setSidebarWrapperRef.bind(this);
+        this.handleClickOutsideSidebar = this.handleClickOutsideSidebar.bind(this);
+        this.handleClickOutsideLanguageSelectorList = this.handleClickOutsideLanguageSelectorList.bind(this);
     }
+
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClickOutsideSidebar);
+        document.addEventListener('mousedown', this.handleClickOutsideLanguageSelectorList);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickOutsideSidebar);
+        document.removeEventListener('mousedown', this.handleClickOutsideLanguageSelectorList);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.location.pathname !== this.props.location.pathname) {
+            if (this.state.showSidebar) {
+                this.hideSidebar();
+            }
+        }
+    }
+
+    handleShowSidebarClick() {
+        this.setState({ showSidebar: true });
+    }
+
+    handleClickOutsideSidebar(event) {
+        if (this.sidebarWrapperRef && !this.sidebarWrapperRef.contains(event.target) && this.state.showSidebar) {
+            this.hideSidebar();
+        }
+    }
+
+    handleClickOutsideLanguageSelectorList(event) {
+        if (this.languageSelectorListWrapperRef && !this.languageSelectorListWrapperRef.contains(event.target) && this.state.showLanguageSelectorList) {
+            this.hideLanguageSelectorList();
+        }
+    }
+
+    hideSidebar() {
+        this.setState({hidingSidebar: true});
+        setTimeout(function() {
+          this.setState({showSidebar: false, hidingSidebar: false});
+        }.bind(this), 225);
+      }
+
+    setSidebarWrapperRef(node) {
+        this.sidebarWrapperRef = node;
+    }
+
+
 
     renderLanguageSelectorButton(availableLanguages, multilingualRoutes, selectedLanguageKey) {
         const hasAvailableLanguages = availableLanguages && Object.keys(availableLanguages).length;
@@ -44,6 +104,7 @@ class NavigationBar extends Component {
     }
 
     render() {
+
         return (
             <div className={style.navigationBar}>
                 <Link to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}`} aria-label='Link Villa Norangdal home page' title='Link Villa Noranngdal home page'>
@@ -51,37 +112,79 @@ class NavigationBar extends Component {
                         <NorangdalVillaLogo alt="Villa Norangdal logo" width="30" height="40" />
                     </span>
                 </Link>
-                <ul className={style.navigationBarLinks}>
-                    <li>
-                        <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}hotel/`} activeClassName={style.activeLink} title={this.props.selectedLanguageKey === 'en' ? 'Hotellet' : 'Hotellet'}>
-                            {this.props.selectedLanguageKey === 'en' ? 'Hotel' : 'Hotellet'}
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}activities/`} activeClassName={style.activeLink} title='Activities'>
-                            {this.props.selectedLanguageKey === 'en' ? 'Activities' : 'Aktiviteter'}
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}gallery/`} activeClassName={style.activeLink} title='Gallery'>
-                            {this.props.selectedLanguageKey === 'en' ? 'Gallery' : 'Galleri'}
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}booking/`} activeClassName={style.activeLink} title='Booking'>
-                            {this.props.selectedLanguageKey === 'en' ? 'Booking' : 'Booking'}
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}hellesylt/`} activeClassName={style.activeLink} title='Hellesylt Boutique Bar'>
-                            {this.props.selectedLanguageKey === 'en' ? 'Hellesylt Boutique & Bar' : 'Hellesylt Boutique & Bar'}
-                        </NavLink>
-                    </li>
-                </ul>
-                <div className={style.languageSelectorListContainer}>
-                    <div ref={this.setLanguageSelectorListWrapperRef} className={`${style.languageSelectorList}`}>
-                        {this.renderLanguageSelectorButton(this.props.availableLanguages, this.props.multilingualRoutes, this.props.selectedLanguageKey)}
+                <span onClick={() => this.handleShowSidebarClick()} className={style.menuButton}>
+                    <MenuIcon className={style.menuIcon} />
+                </span>
+                <div className={`${style.sidebarOverlay} ${this.state.showSidebar
+                    ? style.active
+                    : ''} ${this.state.hidingSidebar
+                        ? style.hidingSidebar
+                        : ''} `}>
+                    <div ref={this.setSidebarWrapperRef} className={style.sidebarContent}>
+                        <div className={style.sidebarContentHeader}>
+                            <ul className={style.sidebarContent}>
+                                <li>
+                                    <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}hotel/`} activeClassName={style.activeLink} title={this.props.selectedLanguageKey === 'en' ? 'Hotellet' : 'Hotellet'}>
+                                        {this.props.selectedLanguageKey === 'en' ? 'Hotel' : 'Hotellet'}
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}activities/`} activeClassName={style.activeLink} title='Activities'>
+                                        {this.props.selectedLanguageKey === 'en' ? 'Activities' : 'Aktiviteter'}
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}gallery/`} activeClassName={style.activeLink} title='Gallery'>
+                                        {this.props.selectedLanguageKey === 'en' ? 'Gallery' : 'Galleri'}
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}booking/`} activeClassName={style.activeLink} title='Booking'>
+                                        {this.props.selectedLanguageKey === 'en' ? 'Booking' : 'Booking'}
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}hellesylt/`} activeClassName={style.activeLink} title='Hellesylt Boutique Bar'>
+                                        {this.props.selectedLanguageKey === 'en' ? 'Hellesylt Boutique & Bar' : 'Hellesylt Boutique & Bar'}
+                                    </NavLink>
+                                </li>
+                            </ul>
+                            <div className={style.languageSelectorListContainer}>
+                                <div ref={this.setLanguageSelectorListWrapperRef} className={`${style.languageSelectorList}`}>
+                                    {this.renderLanguageSelectorButton(this.props.availableLanguages, this.props.multilingualRoutes, this.props.selectedLanguageKey)}
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <div className={style.navigationBar}>
+                    <ul className={style.navigationBarLinks}>
+                        <li>
+                            <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}hotel/`} activeClassName={style.activeLink} title={this.props.selectedLanguageKey === 'en' ? 'Hotellet' : 'Hotellet'}>
+                                {this.props.selectedLanguageKey === 'en' ? 'Hotel' : 'Hotellet'}
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}activities/`} activeClassName={style.activeLink} title='Activities'>
+                                {this.props.selectedLanguageKey === 'en' ? 'Activities' : 'Aktiviteter'}
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}gallery/`} activeClassName={style.activeLink} title='Gallery'>
+                                {this.props.selectedLanguageKey === 'en' ? 'Gallery' : 'Galleri'}
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}booking/`} activeClassName={style.activeLink} title='Booking'>
+                                {this.props.selectedLanguageKey === 'en' ? 'Booking' : 'Booking'}
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`/${this.props.getLanguageSlug(this.props.selectedLanguageKey)}hellesylt/`} activeClassName={style.activeLink} title='Hellesylt Boutique Bar'>
+                                {this.props.selectedLanguageKey === 'en' ? 'Hellesylt Boutique & Bar' : 'Hellesylt Boutique & Bar'}
+                            </NavLink>
+                        </li>
+                    </ul>
                 </div>
             </div>
         )
@@ -91,7 +194,8 @@ class NavigationBar extends Component {
 const mapStateToProps = state => ({
     availableLanguages: state.availableLanguages,
     multilingualRoutes: state.multilingualRoutes,
-    selectedLanguageKey: state.selectedLanguageKey
+    selectedLanguageKey: state.selectedLanguageKey,
+    location: state.router.location
 });
 
 const mapDispatchToProps = {
